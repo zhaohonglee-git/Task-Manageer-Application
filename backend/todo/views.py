828a66a798a5e6805a73bf448d821967db96547e
zhaohonglee <296222105@qq.com>
2021-12-01 17:40:01 +0800

    first commit

---
+++ backend/todo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all().order_by('id')
+    serializer_class = TodoSerializer
